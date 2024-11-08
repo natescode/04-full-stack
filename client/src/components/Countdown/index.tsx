f19ad885a52e7ba76808a57da7973ffa35229b84
natescode <nate@natescode.com>
2024-11-07 20:27:48 -0600

    client

--- client/src/components/Countdown/index.tsx
+++ client/src/components/Countdown/index.tsx
@@ -8,7 +8,7 @@ const Countdown = ({ guesses, isCorrect, hasGuessed }: CountdownProps) => {
   console.log('isCorrect', isCorrect)
   return (
     <div className="countdown flex-row justify-space-evenly">
-      <h3 data-cy="countdown">Guesses Remaining: <span id="guesses-remaining">{guesses}</span></h3>
+      <h3 data-cy="countdown">GUESSES REMAINING: <span id="guesses-remaining">{guesses}</span></h3>
       {hasGuessed && (
         <h3 data-cy="toast">
           {isCorrect ? <span className="toast-success">Correct!</span> : <span className="toast-fail">Incorrect!</span>}
