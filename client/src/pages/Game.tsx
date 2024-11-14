1ea4d572a56ebcdeab1791152a247bc2c5f85f76
natescode <nate@natescode.com>
2024-11-13 19:32:22 -0600

    WIP

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -76,7 +76,7 @@ const GamePage = () => {
 
   return (
     <main>
-    <h1>HI</h1>
+    <h1>Hello</h1>
       <div className="flex-row justify-center" data-cy="game">
         <div
           className="col mb-3 p-3"
